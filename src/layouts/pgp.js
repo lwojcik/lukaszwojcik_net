bcd44fc02eb900b8ddb7ee55816c3f35f807fd1f
Lukasz Wojcik <kontakt@lukaszwojcik.net>
2018-01-18 00:38:26 +0100

    adding site code

--- src/layouts/pgp.js
+++ src/layouts/pgp.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
+import Header from '../components/Header'
 import './index.css'
 
 const TemplateWrapper = ({ children }) => (
@@ -13,8 +14,17 @@ const TemplateWrapper = ({ children }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-
-    {children()}
+    <Header />
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '0px 1.0875rem 1.45rem',
+        paddingTop: 0,
+      }}
+    >
+      {children()}
+    </div>
   </div>
 )
 
