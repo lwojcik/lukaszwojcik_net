bb18e7c9538428ca5b9d39a8f11b89d80e948411
Łukasz Wójcik <lwojcik@lukem.net>
2020-11-25 23:08:33 +0100

    add css-camera sample

--- _includes/scripts/scripts.js
+++ _includes/scripts/scripts.js
@@ -1,6 +1,58 @@
 (() => {
+  const camera = () => {
+    const windowWidth = document.body.offsetWidth;
+    const cardButton = document.querySelector("#card-button");
+    const codeButton = document.querySelector("#code-button");
+    const heroButton = document.querySelector("#hero-button");
+    const moreButton = document.querySelector("#more-button");
+    const buttons = [
+      cardButton, codeButton, heroButton, moreButton,
+    ];
+
+    const card = document.querySelector("#card");
+    const code = document.querySelector("#code");
+    const hero = document.querySelector("#hero");
+    const more = document.querySelector("#more");
+
+    const restoreButtons = () => {
+      buttons.forEach(button => button.classList.add("is-outlined"));
+    }
+
+    const camera = new CSSCamera("#space", {
+      perspective: windowWidth / 2,
+    });
+
+    camera.focus(hero);
+    camera.update(2000);
+
+    cardButton.onclick = () => {
+      camera.focus(card);
+      camera.update(2000);
+      restoreButtons();
+      cardButton.classList.remove("is-outlined");
+    }
+    codeButton.onclick = () => {
+      camera.focus(code);
+      camera.update(2000);
+      restoreButtons();
+      codeButton.classList.remove("is-outlined");
+    }
+    heroButton.onclick = () => {
+      camera.focus(hero);
+      camera.update(2000);
+      restoreButtons();
+      heroButton.classList.remove("is-outlined");
+    }
+    moreButton.onclick = () => {
+      camera.focus(more);
+      camera.update(2000);
+      restoreButtons();
+      moreButton.classList.remove("is-outlined");
+    }
+  }
+
   const start = () => {
-    console.log('scripts start')
+    camera();
   }
 
   start();
